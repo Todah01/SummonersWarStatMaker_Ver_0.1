5348f3a5f40280d3c55271d46b4ab57fc82129f4
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-09-27 22:05:22 +0900

    2022.09.27
    make start scene and irene animation

---
+++ Assets/Script/loading_manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+public class loading_manager : MonoBehaviour
+{
+    public GameObject irene_animation;
+
+    Animator irene_anim_controllor;
+    bool isStart = false;
+
+    private void Awake()
+    {
+        irene_anim_controllor = irene_animation.GetComponent<Animator>();
+    }
+
+    public void OnClickIrene()
+    {
+        Debug.Log("Click Irene");
+        //if (isStart) return;
+
+        //isStart = true;
+        irene_anim_controllor.SetTrigger("IsMotion");
+    }
+}
