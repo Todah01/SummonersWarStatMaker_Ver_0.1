47b8be7969e38faac6fc136a458350a564590013
Todah01 <57752105+Todah01@users.noreply.github.com>
2022-09-30 17:18:39 +0900

    2022.09.30
    develop warrior anim and fix calculate start logic

--- Assets/Script/etc_window_control.cs
+++ Assets/Script/etc_window_control.cs
@@ -6,12 +6,21 @@ public class etc_window_control : MonoBehaviour
 {
     public GameObject etc_window;
     public GameObject etc_angel;
+    public GameObject etc_start;
+    public GameObject select_data;
 
+    Animator warrior_anim;
     Animator angelmon_anim;
     bool isetcopen = false;
     private void Awake()
     {
+        warrior_anim = etc_start.GetComponent<Animator>();
         angelmon_anim = etc_angel.GetComponent<Animator>();
+    }
+    public void OnClickStartBtn()
+    {
+        warrior_anim.SetTrigger("IsMotion");
+        StartCoroutine(CalculateStart());
     }
     public void EtcWindowControl(bool control)
     {
@@ -28,6 +37,11 @@ public class etc_window_control : MonoBehaviour
     public void RecalculateEtc()
     {
         StartCoroutine(ReCalculateStart());
+    }
+    IEnumerator CalculateStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        select_data.GetComponent<select_data_control>().Cal_Start();
     }
     IEnumerator ReCalculateStart()
     {
